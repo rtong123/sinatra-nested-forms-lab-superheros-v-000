827f3324b119f39c7c4136aa774277e5abc62b08
Rebecca Tong <rtong@buffalo.edu>
2018-07-15 22:37:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,14 @@ class App < Sinatra::Base
       @teamname = params[:team][:name]
       @teammotto = params[:team][:motto]
       @heroname1 = params[:hero][0][:name]
-      @heropower1 = params[:hero][1][:power]
-      @herobio1 = params[:hero][2][:biography]
-      @heroname2 = params[:hero][0][:name]
+      @heropower1 = params[:hero][0][:power]
+      @herobio1 = params[:hero][0][:biography]
+      
+      @heroname2 = params[:hero][1][:name]
       @heropower2 = params[:hero][1][:power]
-      @herobio2 = params[:hero][2][:biography]
-      @heroname3 = params[:hero][0][:name]
-      @heropower3 = params[:hero][1][:power]
-      binding.pry
+      @herobio2 = params[:hero][1][:biography]
+      @heroname3 = params[:hero][2][:name]
+      @heropower3 = params[:hero][2][:power]
       @herobio13 = params[:hero][2][:biography]
 
       erb :team
