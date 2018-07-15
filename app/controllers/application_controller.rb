bfbe836784117788180082dc67911a36c710e350
Rebecca Tong <rtong@buffalo.edu>
2018-07-15 22:38:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,18 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-      
+      @teamname = params[:team][:name]
+      @teammotto = params[:team][:motto]
+      @heroname1 = params[:hero][0][:name]
+      @heropower1 = params[:hero][0][:power]
+      @herobio1 = params[:hero][0][:biography]
+      @heroname2 = params[:hero][1][:name]
+      @heropower2 = params[:hero][1][:power]
+      @herobio2 = params[:hero][1][:biography]
+      @heroname3 = params[:hero][2][:name]
+      @heropower3 = params[:hero][2][:power]
+      @herobio3 = params[:hero][2][:biography]
+
       erb :team
     end
 end
